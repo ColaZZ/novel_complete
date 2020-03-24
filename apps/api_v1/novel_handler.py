0e2819446ceeaf2e449f34763cf7521d929c4de5
cheung <zlinxx@foxmail.com>
2020-03-24 18:32:08 +0800

    fix br 转义符

--- apps/api_v1/novel_handler.py
+++ apps/api_v1/novel_handler.py
@@ -27,7 +27,7 @@ class CompleteHandler(FoundHandler):
         tree = etree.HTML(content)
         path_content = '//div[@id="main"]/div[@id="content"]/text()'
         node_content = tree.xpath(path_content)
-        node_text = "".join(node_content)
+        node_text = "<br><br>".join(node_content)
 
         temp_path_base = str(url[33:])
         tp_list = temp_path_base.split("/")
@@ -35,12 +35,14 @@ class CompleteHandler(FoundHandler):
         temp_path = tp_list[1]
         chapter_url_base = tp_list[-1]
         cur_path = "/volume/novel_context" + os.path.sep + "35kushu.com"
+        # cur_path = "/mnt/d/"
         target_path = cur_path + os.path.sep + str(category_id) + os.path.sep + temp_path
         filename_path = cur_path + os.path.sep + str(category_id) + os.path.sep + temp_path + os.path.sep + \
                         str(chapter_url_base[:-5]) + '.txt'
 
         # print(target_path, filename_path)
 
+        # print(node_text)
         if not os.path.exists(target_path):
             os.makedirs(target_path)
         with open(filename_path, 'w', encoding='utf-8') as f:
